90eb908d28c30b5e1d8ecf6a47a41c89ccd8d1db
James Fray <edh2375ank@gmail.com>
2016-09-22 11:31:54 +0330

    fixes #4

--- builds/unpacked/firefox/lib/firefox/firefox.js
+++ builds/unpacked/firefox/lib/firefox/firefox.js
@@ -9,9 +9,13 @@ var self        = require('sdk/self'),
     pageMod     = require('sdk/page-mod'),
     tabs        = require('sdk/tabs'),
     timers      = require('sdk/timers'),
-    contextMenu = require('sdk/context-menu'),
+    desktop     = require('sdk/system').platform !== 'android',
     array       = require('sdk/util/array'),
-    {resolve, defer}     = require('sdk/core/promise');
+    unload      = require('sdk/system/unload'),
+    {resolve, defer}     = require('sdk/core/promise'),
+    {Cu}        = require('chrome');
+
+var {Services} = Cu.import('resource://gre/modules/Services.jsm');
 
 var workers = [], callbacks = [];
 pageMod.PageMod({
@@ -106,28 +110,44 @@ exports.tab = {
 
 exports.context_menu = {
   create: function (title, callback) {
-    contextMenu.Item({
-      label: title,
-      image: data.url('./icons/16.png'),
-      context: contextMenu.SelectorContext('a'),
-      contentScript: 'self.on("click", function (node, data) {' +
-                     '  self.postMessage(node.href);' +
-                     '});',
-      onMessage: function (url) {
-        callback(url);
-      }
-    });
-    contextMenu.Item({
-      label: title,
-      image: data.url('./icons/16.png'),
-      context: contextMenu.PredicateContext(o => /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/.test(o.selectionText)),
-      contentScript: 'self.on("click", function () {' +
-                     '  self.postMessage(window.getSelection().toString());' +
-                     '});',
-      onMessage: function (url) {
-        callback(url);
-      }
-    });
+    if (desktop) {
+      let contextMenu = require('sdk/context-menu');
+      contextMenu.Item({
+        label: title,
+        image: data.url('./icons/16.png'),
+        context: contextMenu.SelectorContext('a'),
+        contentScript: 'self.on("click", function (node, data) {' +
+                       '  self.postMessage(node.href);' +
+                       '});',
+        onMessage: function (url) {
+          callback(url);
+        }
+      });
+      contextMenu.Item({
+        label: title,
+        image: data.url('./icons/16.png'),
+        context: contextMenu.PredicateContext(o => /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/.test(o.selectionText)),
+        contentScript: 'self.on("click", function () {' +
+                       '  self.postMessage(window.getSelection().toString());' +
+                       '});',
+        onMessage: function (url) {
+          callback(url);
+        }
+      });
+    }
+    else {
+      let window = Services.wm.getMostRecentWindow('navigator:browser');
+
+      let id = window.NativeWindow.contextmenus.add(
+        title,
+        window.NativeWindow.contextmenus.SelectorContext('a'),
+        (target) => callback(target.href)
+      );
+
+      unload.when(() => {
+        window.NativeWindow.contextmenus.remove(id);
+      });
+    }
   }
 };
 
